958cec2c51bc1bdbba0381bc50d9ee5d6ac9c14e
Bibek Thapa <bibekthapa00f@gmail.com>
2023-09-11 09:44:17 +0545

    Final Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// URL
+let url = "https://icanhazdadjoke.com/";
+
+// Ascessing the DOM
+const button = document.querySelector("button");
+const result = document.querySelector(".result");
+
+// Fetching data
+const fetchData = async () => {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "Application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("error");
+    }
+    let data = await response.json();
+    result.innerHTML = data.joke
+  } catch (error) {
+    console.log(error);
+    result.textContent = "An error occured";
+  }
+};
+
+fetchData();
+
+button.addEventListener("click",fetchData)
